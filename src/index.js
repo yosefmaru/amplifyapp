94199aa6523987727fa97f1b35eb2fbdb5d6692e
Yosef <yosefmaru@gmail.com>
2020-08-21 10:06:31 -0700

    modify index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
-import config from './aws-exports';
+import config from './aws-exports.js';
 Amplify.configure(config);
 
 ReactDOM.render(
